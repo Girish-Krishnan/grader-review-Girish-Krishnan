751a0dd7ff1c599e3bc63077b836425378498ce6
Girish Krishnan <gikrishnan@ucsd.edu>
2023-11-27 16:30:40 -0800

    try on ieng6

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,137 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500) 
+  public void testMergeLeftEnd() {
+    List<String> left = Arrays.asList("c", "d", "a");
+    List<String> right = Arrays.asList("a", "b", "a");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter1() {
+    List<String> input = Arrays.asList("Moon", "moon", "Sun", "sun");
+    List<String> filtered = ListExamples.filter(input, new IsMoon());
+    List<String> expected = Arrays.asList("Moon", "moon");
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter2() {
+    List<String> input = Arrays.asList("Moon", "moon", "Sun", "sun");
+    List<String> filtered = ListExamples.filter(input, new StringChecker() {
+      public boolean checkString(String s) {
+        return s.equalsIgnoreCase("sun");
+      }
+    });
+    List<String> expected = Arrays.asList("Sun", "sun");
+    assertEquals(expected, filtered);
+  }
+
+  // Add a test to test filter, but call filter twice in a row with two different inputs to see if both are correct
+  @Test(timeout = 500)
+  public void testFilter3() {
+    List<String> input = Arrays.asList("Moon", "moon", "Sun", "sun");
+    List<String> filtered = ListExamples.filter(input, new StringChecker() {
+      public boolean checkString(String s) {
+        return s.equalsIgnoreCase("sun");
+      }
+    });
+    List<String> expected = Arrays.asList("Sun", "sun");
+    
+    List<String> input2 = Arrays.asList("Moon", "moon", "Sun", "sun");
+    List<String> filtered2 = ListExamples.filter(input2, new StringChecker() {
+      public boolean checkString(String s) {
+        return s.equalsIgnoreCase("moon");
+      }
+    });
+    List<String> expected2 = Arrays.asList("Moon", "moon");
+
+    assertEquals(expected, filtered);
+    assertEquals(expected2, filtered2);
+  }
+
+  // Test with empty list
+  @Test(timeout = 500)
+  public void testFilter4() {
+    List<String> input = Arrays.asList();
+    List<String> filtered = ListExamples.filter(input, new StringChecker() {
+      public boolean checkString(String s) {
+        return s.equalsIgnoreCase("sun");
+      }
+    });
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
+  // Test with empty list followed by non-empty list, and see that both are correct
+  @Test(timeout = 500)
+  public void testFilter5() {
+    List<String> input = Arrays.asList();
+    List<String> filtered = ListExamples.filter(input, new StringChecker() {
+      public boolean checkString(String s) {
+        return s.equalsIgnoreCase("sun");
+      }
+    });
+    List<String> expected = Arrays.asList();
+
+    List<String> input2 = Arrays.asList("Moon", "moon", "Sun", "sun");
+    List<String> filtered2 = ListExamples.filter(input2, new StringChecker() {
+      public boolean checkString(String s) {
+        return s.equalsIgnoreCase("moon");
+      }
+    });
+    List<String> expected2 = Arrays.asList("Moon", "moon");
+
+    assertEquals(expected, filtered);
+    assertEquals(expected2, filtered2);
+  }
+
+  // Test merged with empty lists, and with one empty and one non-empty list
+  @Test(timeout = 500)
+  public void testMergeEmpty() {
+    List<String> left = Arrays.asList();
+    List<String> right = Arrays.asList();
+    List<String> merged = ListExamples.merge(left, right);
+
+    List<String> left2 = Arrays.asList();
+    List<String> right2 = Arrays.asList("a", "b", "c");
+    List<String> merged2 = ListExamples.merge(left2, right2);
+
+    List<String> expected = Arrays.asList();
+    List<String> expected2 = Arrays.asList("a", "b", "c");
+
+    assertEquals(expected, merged);
+    assertEquals(expected2, merged2);
+  }
+
+  // Test filter with some other string checkers, maybe try an empty string checker
+  @Test(timeout = 500)
+  public void testFilter6() {
+    List<String> input = Arrays.asList("Moon", "moon", "Sun", "sun");
+    List<String> filtered = ListExamples.filter(input, new StringChecker() {
+      public boolean checkString(String s) {
+        return s.equalsIgnoreCase("");
+      }
+    });
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
+  // Test filter with some other string checkers
+  @Test(timeout = 500)
+  public void testFilter7() {
+    List<String> input = Arrays.asList("M", "A", "D", "C");
+    List<String> filtered = ListExamples.filter(input, new StringChecker() {
+      public boolean checkString(String s) {
+        return s.equalsIgnoreCase("A");
+      }
+    });
+    List<String> expected = Arrays.asList("Moon", "moon");
+    assertEquals(expected, filtered);
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -14,3 +14,25 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# Check if student-submission/ListExamples.java exists
+if [ ! -f student-submission/ListExamples.java ]; then
+    echo "[ERROR] ListExamples.java not found in the root of the repository. Please make sure that the file is in the root of the repository and try again. Also, check that the file is named correctly."
+    exit 1
+fi
+
+# Else, move it to the grading area and compile it
+mv student-submission/ListExamples.java grading-area
+
+# Try compiling ListExamples.java and store the result in a variable
+# If the compilation fails, print an error message indicating what the compilation error was
+# and exit with a non-zero exit code
+javac -cp $CPATH grading-area/ListExamples.java
+if [ $? -ne 0 ]; then
+    echo "[ERROR] ListExamples.java failed to compile. Please make sure that the file compiles and try again. The error message is shown above."
+    exit 1
+fi
+
+# Else, run the tests on ListExamples.class
+javac -cp $CPATH TestListExamples.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
